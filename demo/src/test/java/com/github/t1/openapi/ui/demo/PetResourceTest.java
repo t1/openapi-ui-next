008f99510abb1949538f5fc0646a49629aa62fc1
Ruediger zu Dohna <snackbox@sinntr.eu>
2026-03-14 06:54:08 +0100

    add PetResource tests

---
+++ demo/src/test/java/com/github/t1/openapi/ui/demo/PetResourceTest.java
@@ -0,0 +1,39 @@
+package com.github.t1.openapi.ui.demo;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+@QuarkusTest
+class PetResourceTest {
+    @Test
+    void shouldListPets() {
+        given()
+                .when().get("/pets")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(2))
+                .body("[0].name", is("Max"))
+                .body("[1].name", is("Bella"));
+    }
+
+    @Test
+    void shouldGetPetById() {
+        given()
+                .when().get("/pets/1")
+                .then()
+                .statusCode(200)
+                .body("name", is("Max"))
+                .body("status", is("available"));
+    }
+
+    @Test
+    void shouldReturn404ForUnknownPet() {
+        given()
+                .when().get("/pets/999")
+                .then()
+                .statusCode(404);
+    }
+}
