465b3236d29b7da13800904a7f9df1f7cd9db75e
Ruediger zu Dohna <snackbox@sinntr.eu>
2026-03-14 14:28:22 +0100

    add contact and summary to demo @Info annotation

--- demo/src/main/java/com/github/t1/openapi/ui/demo/App.java
+++ demo/src/main/java/com/github/t1/openapi/ui/demo/App.java
@@ -3,10 +3,13 @@ package com.github.t1.openapi.ui.demo;
 import jakarta.ws.rs.core.Application;
 
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 
 @OpenAPIDefinition(info = @Info(
         title = "Pet Store",
+        summary = "Demo pet store for the OpenAPI UI generator",
         version = "1.0.0",
-        description = "A simple pet store API for demonstrating the OpenAPI UI generator."))
+        description = "A simple pet store API for demonstrating the OpenAPI UI generator.",
+        contact = @Contact(name = "OpenAPI UI", url = "https://github.com/t1/openapi-ui-next")))
 public class App extends Application {}
